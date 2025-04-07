f5d690e020711225c43374faf8c81cf4d950de7b
syrnxalno <archanakrishna0023@gmail.com>
2025-04-07 21:04:45 +0530

    resize util added

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import fetchMetaData from "./utils/readImage.js";
+import resizeImage from "./utils/resizeImage.js";
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -10,6 +11,15 @@ app.get('/', async(req,res)=>{
     try{
     const metadata = await fetchMetaData();
     res.json(metadata);
+    }
+    catch(err){
+        res.send("Error displaying image metadata :"+err);
+    }
+})
+app.get('/resize', async(req,res)=>{
+    try{
+    const resize = await resizeImage();
+    res.json(resize);
     }
     catch(err){
         res.send("Error displaying image metadata :"+err);
---
+++ src/utils/resizeImage.js
@@ -0,0 +1,13 @@
+import sharp from 'sharp'
+
+async function resizeImage(){
+    try{
+       const resizeImg =  await sharp('src/images/test.png').resize({width:120, height:120})
+       .toFile('src/test-resize/one.png');
+       return resizeImg;
+    }
+    catch(err){
+        return err;
+    }
+}
+export default resizeImage;
