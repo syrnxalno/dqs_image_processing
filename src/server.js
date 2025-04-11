99ef43a8f90b7f795b0c1cf22257ec462ea494f3
syrnxalno <archanakrishna0023@gmail.com>
2025-04-11 21:40:48 +0530

    github oauth layout

--- src/server.js
+++ src/server.js
@@ -1,11 +1,25 @@
 import imageRoutes from './routes/imageRoutes.js'
 import express from 'express';
 import dotenv from 'dotenv';
+import session from 'express-session'; //for later session additions
+import passport from 'passport';
+import './config/passport.js'; //to load passport
+import authRoutes from './routes/oauthRoutes.js';
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
 app.use('/', imageRoutes);
+app.use('/', authRoutes);
+
+
 app.listen(PORT, () =>{
 console.log(`Server running on port ${PORT}`)
 })
