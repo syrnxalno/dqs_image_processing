d2859190e3b77ca47f99c567be3e1a0193bfe0cd
syrnxalno <archanakrishna0023@gmail.com>
2025-05-01 14:36:37 +0530

    feat : queue dashboard setup

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,17 +1,23 @@
-import imageRoutes from './routes/imageRoutes.js'
 import express from 'express';
 import dotenv from 'dotenv';
-import sessionMiddleware from './middlewares/sessionMiddleware.js'
+import sessionMiddleware from './middlewares/sessionMiddleware.js';
 import passport from 'passport';
-import './config/passportConfig.js'; //to load passport
+import './config/passportConfig.js';
+
+import imageRoutes from './routes/imageRoutes.js';
 import authRoutes from './routes/oauthRoutes.js';
+import bullboardRoutes from './routes/bullboardRoutes.js';
+
+import './workers/resizeWorker.js';
+import './workers/compressWorker.js';
+import './workers/watermarkWorker.js';
+import './workers/saveimageWorker.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Passport middleware
 app.use(passport.initialize());
 sessionMiddleware(app);
 app.use(passport.session());
@@ -19,9 +25,9 @@ app.use(passport.session());
 // Routes
 app.use('/', imageRoutes);
 app.use('/', authRoutes);
+app.use('/', bullboardRoutes); // Bull Board UI
 
 
-app.listen(PORT, () =>{
-console.log(`Server running on port ${PORT}`)
-})
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
