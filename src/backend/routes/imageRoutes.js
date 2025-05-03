ab37cde11754013bb82237c8c9c1c549136fc03f
syrnxalno <archanakrishna0023@gmail.com>
2025-05-03 16:36:37 +0530

    feat(upload): image upload and processing

--- src/backend/routes/imageRoutes.js
+++ src/backend/routes/imageRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import {processImagePipeline} from '../controllers/imageController.js'
+import multer from 'multer'
+import cors from 'cors'
 const router = express.Router();
+const upload = multer({ dest: 'uploads/' })
+router.use(cors());
 
-
-router.get('/process', processImagePipeline);
+//POST route to start pipeline
+router.post('/admin/upload', upload.single('image'), processImagePipeline);
 
 export default router;
